e7c113f267afc1a9b85ea804102e6cc654b4b639
Nolan Hewitt <nolanhewitt@aol.com>
2019-09-24 19:06:43 -0500

    added program later than I should have

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ("Big Burger");
+INSERT INTO burgers (name) VALUES ("Medium Burger");
+INSERT INTO burgers (name) VALUES ("Small Burger");
+INSERT INTO burgers (name) VALUES ("Waffle Burger");
+INSERT INTO burgers (name) VALUES ("Rude Burger");
